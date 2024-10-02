fa33bf8393c4d6ce33cd333089604efb9a4bb855
Swapnil <swapnilp161998@gmail.com>
2024-10-02 23:42:31 +0530

    Learning Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
+import React ,{ useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  let [counter , setcount] = useState(15)
+
+  // const addValue = () => {
+  //   console.log("Clicked", counter);
+  //   // ?counter = counter + 1
+  //   setCounter(counter + 1)
+  // }
+
+  // const removeValue = () => {
+  //   setCounter(counter - 1)
+  // }
+
+  const addcnt = () =>{
+    if(counter < 20){
+      setcount(counter + 1)
+    }
+  }
+  const remcnt = () =>{
+    if(counter > 0){
+      setcount(counter - 1)
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>Code with Swapnil</h1>
+     <h2>Counter Value : {counter}</h2>
+
+     <button
+     onClick={addcnt}
+     >Add Value : {counter}</button>
+     <br />
+     <button
+     onClick={remcnt}
+     >Remove Value : {counter}</button>
+     <p>footer : {counter}</p>
     </>
   )
 }
